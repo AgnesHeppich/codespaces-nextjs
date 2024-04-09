53075ee60502c38f2bfc73ec48545fdb052ac99a
Agnes Heppich <89513762+AgnesHeppich@users.noreply.github.com>
2024-04-09 08:38:35 +0000

    Add await to fix error lol

--- app/test/route.js
+++ app/test/route.js
@@ -2,13 +2,13 @@ export async function GET() {
     
     const maratondata = await fetch("https://secure.onreg.com/onreg2/bibexchange/?eventid=6087&language=us")
     const html = await maratondata.text()
-    if(!html.includes("There are currently no race numbers for sale. Try again later.")) {
-      console.log("Det finns biljetter")
+    if(html.includes("There are currently no race numbers for sale. Try again later.")) {
+      console.log("Det finns inte biljetter")
       // Post to slack
       await fetch("https://hooks.slack.com/services/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", {
         method: "POST",
         body: JSON.stringify({
-          text: "Det finns biljetter bror, walla. Köpa fan, sälj beggat. Paya jacke!"
+          text: "Det här är ett test. Ignorera detta"
           
       })})
     }
